5856514b00101f69b1320bfa38093c7cfaadda06
Shreyash Daundkar <productiveappa@gmail.com>
2023-08-15 17:30:42 +0530

    Added functionality to store users data at local storage

--- script.js
+++ script.js
@@ -7,27 +7,31 @@ const msg = document.querySelector('.msg');
 
 
 
+
 // Managing form Events
 
 form.addEventListener('submit', onSubmit);
 function onSubmit(e) {
     e.preventDefault();
-    if(!validation()) return
+    if(!isValid()) return;
+    storeLocally(name.value, email.value);
+    showMsg('success', 'Submitted');
+    name.value = '';
+    email.value = '';
 }
+
+
 
 
 
 // Validation
 
-function validation() {
+function isValid() {
     const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,10}$/;
     if(name.value.trim().length < 2 && !emailPattern.test(email.value)) showMsg('error', 'Name and Email is invalid');
     else if(name.value.trim().length < 2) showMsg('error', 'Name is invalid');
     else if(!emailPattern.test(email.value)) showMsg('error', 'Email is invalid');
-    else {
-        showMsg('success', 'Submitted');
-        return true;
-    }
+    else return true;
 }
 
 function showMsg(result, text) {
@@ -39,3 +43,12 @@ function showMsg(result, text) {
     }, 3000);
 }
 
+
+
+
+
+// Store in local storage
+
+function storeLocally(name, email) {
+     localStorage.setItem(name.toLowerCase(), JSON.stringify({name, email}));
+}
